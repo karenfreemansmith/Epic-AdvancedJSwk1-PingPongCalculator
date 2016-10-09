a274d7b094b5ac3c998f3a7fa8b6619f95f93de6
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-10-08 22:42:40 -0700

    split pingpong logic into two files with exports and require

---
+++ backend.js
@@ -0,0 +1,35 @@
+function Calculator(skinName) {
+  this.skin = skinName;
+}
+
+Calculator.prototype.playPong = function(userNumber) {
+  var message=[];
+  if(userNumber<0) {
+    for (i=-1; i>=userNumber; i--) {
+      if (i%15===0) {
+        message.push("ping-pong");
+      } else if (i%5===0) {
+        message.push("pong");
+      } else if (i%3===0) {
+        message.push("ping");
+      } else {
+        message.push(i);
+      }
+    }
+  } else {
+    for (i=1; i<=userNumber; i++) {
+      if (i%15===0) {
+        message.push("ping-pong");
+      } else if (i%5===0) {
+        message.push("pong");
+      } else if (i%3===0) {
+        message.push("ping");
+      } else {
+        message.push(i);
+      }
+    }
+  }
+  return message;
+ }
+
+exports.calculatorModule = Calculator;

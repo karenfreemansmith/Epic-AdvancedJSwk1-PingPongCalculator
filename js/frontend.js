9b15dedbb2440560f9a3df69bbcb05df64753185
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-01-03 16:47:41 -0800

    refactor sounds and gets animation to play on resubmit

--- js/frontend.js
+++ js/frontend.js
@@ -1,29 +1,44 @@
 var Calculator = require('./../js/backend.js').calculatorModule;
+const sndMiss = new Audio("img/miss.mp3");
+const sndSlam1 = new Audio("img/slam1.mp3");
+const sndSlam2 = new Audio("img/slam2.mp3");
+const sndVolley = new Audio("img/volley.mp3");
 
 function playMiss() {
-  new Audio("img/miss.mp3").play();
+  sndMiss.currentTime = 0;
+  sndMiss.play();
 }
 function playPing() {
-  new Audio("img/slam1.mp3").play();
+  acorn.style.animation= "ping 1s linear";
+  acorn.style.webkitAnimation="ping 1s linear";
+  sndSlam1.currentTime = 0;
+  sndSlam1.play();
 }
 function playPong() {
-  new Audio("img/slam2.mp3").play();
+  acorn.style.animation= "pong 1s linear";
+  acorn.style.webkitAnimation="pong 1s linear";
+  sndSlam2.currentTime = 0;
+  sndSlam2.play();
 }
 function playVolley() {
-  new Audio("img/volley.mp3").play();
+  acorn.style.animation= "volley 1s linear";
+  acorn.style.webkitAnimation="volley 1s linear";
+  sndVolley.currentTime = 0;
+  sndVolley.play();
 }
 
 $(document).ready(function() {
   $("form#play").submit(function(event) {
-    var soundTimer=0;
+    var soundTimer=1000;
     event.preventDefault();
     $("#pingpong").empty();
     $("#pingpong").hide();
-    acorn.style.animation= "volley 3s linear";
-    acorn.style.webkitAnimation="volley 3s linear";
+
     var simpleCalculator = new Calculator("hot pink");
     var output = simpleCalculator.playPong(parseInt($("#userNumber").val()));
-    output.forEach(function(item) {
+    output.forEach(item => {
+      acorn.style.animation= "";
+      acorn.style.webkitAnimation="";
       if(item==="ping") {
         setTimeout(playPing, soundTimer);
       } else if(item==="pong") {
@@ -33,8 +48,8 @@ $(document).ready(function() {
       } else {
         setTimeout(playMiss, soundTimer);
       }
-      soundTimer+=500;
-      $("#pingpong").append("<p class='"+item+"'>" + item + "</p>").fadeIn(9999);
+      //soundTimer+=500;
+      $("#pingpong").append("<p class='show'>" + item + "</p>").fadeIn(9999);
     });
   });
 });
